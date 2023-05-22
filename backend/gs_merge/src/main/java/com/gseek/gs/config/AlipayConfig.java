14e9dce4c4a18243020c80e85b3a668fe3fd7970
Sinicaphak <2967306689@qq.com>
2023-05-22 17:12:58 +0800

    项目后端：
    # 新增：
    无
    # 修改：
    
    - 修复了连不上rabbitMQ的问题
    配置文件内容不要直接注入到static字段上，应该使用setter注入
    
    # 重要待办：
    - 注释补齐！！！

--- backend/gs_merge/src/main/java/com/gseek/gs/config/AlipayConfig.java
+++ backend/gs_merge/src/main/java/com/gseek/gs/config/AlipayConfig.java
@@ -12,68 +12,121 @@ public class AlipayConfig {
     /**
      * 应用ID
      */
-    @Value("${alipay.key.sandbox.app_id}")
+
     public static String APP_ID ;
     /**
      * 应用公匙
      * */
-    @Value("${alipay.key.sandbox.merchant_public_key}")
+
     public static String APP_PUBLIC_KEY;
     /**
      * 商户私钥，PKCS8格式RSA2私钥
      */
-    @Value("${alipay.key.sandbox.merchant_private_key}")
+
     public static String MERCHANT_PRIVATE_KEY;
     /**
      * 支付宝公钥
      */
-    @Value("${alipay.key.sandbox.alipay_public_key}")
+
     public static String ALIPAY_PUBLIC_KEY;
     /**
      * 接口加密密匙
      * */
-    @Value("${alipay.key.sandbox.encrypt_key}")
+
     public static String ENCRYPT_KEY;
     /**
      * 商户id
      * */
-    @Value("${alipay.key.sandbox.pid}")
+
     public static String PID;
 
     /**
      * 服务器异步通知页面路径
      */
-    @Value("${alipay.api.notify_url}")
+
     public static String NOTIFY_URL;
     /**
      * 页面跳转同步通知页面路径
      */
-    @Value("${alipay.api.return_url}")
+
     public static String RETURN_URL;
     /**
      * 用户付款中途退出返回商户网站的地址
      * */
-    @Value("${alipay.api.quit_url}")
+
     public static String QUIT_URL;
 
     /**
      * 签名方式
      */
-    @Value("${alipay.common.sign_type}")
-    public static String SIGN_TYPE = "RSA2";
+
+    public static String SIGN_TYPE;
     /**
      * 字符编码格式
      */
-    @Value("${alipay.common.charset}")
-    public static String CHARSET = "utf-8";
+
+    public static String CHARSET;
     /**
      * 支付宝网关
      */
-    @Value("${alipay.common.gatewayUrl}")
-    public static String GATEWAY_URL = "https://openapi.alipay.com/gateway.do";
+
+    public static String GATEWAY_URL;
     /**
      * 支付过期时间
      * */
-    @Value("${alipay.common.timeout}")
+
     public static String TIME_OUT;
+
+    @Value("${alipay.key.sandbox.app_id}")
+    public void setAppId(String appId) {
+        APP_ID = appId;
+    }
+    @Value("${alipay.key.sandbox.merchant_public_key}")
+    public void setAppPublicKey(String appPublicKey) {
+        APP_PUBLIC_KEY = appPublicKey;
+    }
+    @Value("${alipay.key.sandbox.merchant_private_key}")
+    public void setMerchantPrivateKey(String merchantPrivateKey) {
+        MERCHANT_PRIVATE_KEY = merchantPrivateKey;
+    }
+    @Value("${alipay.key.sandbox.alipay_public_key}")
+    public void setAlipayPublicKey(String alipayPublicKey) {
+        ALIPAY_PUBLIC_KEY = alipayPublicKey;
+    }
+    @Value("${alipay.key.sandbox.encrypt_key}")
+    public void setEncryptKey(String encryptKey) {
+        ENCRYPT_KEY = encryptKey;
+    }
+    @Value("${alipay.key.sandbox.pid}")
+    public void setPID(String PID) {
+        AlipayConfig.PID = PID;
+    }
+    @Value("${alipay.api.notify_url}")
+    public void setNotifyUrl(String notifyUrl) {
+        NOTIFY_URL = notifyUrl;
+    }
+    @Value("${alipay.api.return_url}")
+    public void setReturnUrl(String returnUrl) {
+        RETURN_URL = returnUrl;
+    }
+    @Value("${alipay.api.quit_url}")
+    public void setQuitUrl(String quitUrl) {
+        QUIT_URL = quitUrl;
+    }
+    @Value("${alipay.common.sign_type}")
+    public void setSignType(String signType) {
+        SIGN_TYPE = signType;
+    }
+    @Value("${alipay.common.charset}")
+    public void setCHARSET(String CHARSET) {
+        AlipayConfig.CHARSET = CHARSET;
+    }
+    @Value("${alipay.common.gatewayUrl}")
+    public void setGatewayUrl(String gatewayUrl) {
+        GATEWAY_URL = gatewayUrl;
+    }
+    @Value("${alipay.common.timeout}")
+    public void setTimeOut(String timeOut) {
+        TIME_OUT = timeOut;
+    }
 }
