62210b58e51c9d690e5865e6b6faddd6d67424fd
Isabella-Yishan-Ji <2285348924@qq.com>
2023-06-18 22:53:06 +0800

    项目后端：
    删除早期未合并项目

--- backend/gseek0_0/src/main/java/com/gseek/gseek0_0/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.gseek.gseek0_0.service;
-
-import com.gseek.gseek0_0.pojo.UserPassword;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface UserService {
-    UserPassword getUserPassword();
-}
--- backend/gseek0_0/src/main/java/com/gseek/gseek0_0/utils/UUID.java
+++
@@ -1,7 +0,0 @@
-package com.gseek.gseek0_0.utils;
-
-public class UUID {
-    public static String getUUID(){
-        return java.util.UUID.randomUUID().toString().replaceAll("-","");
-    }
-}
